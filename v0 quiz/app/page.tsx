6f512508eb2c57d42efa27b68056eb95f1db2eb4
Stigamortis <clankacentral@gmail.com>
2026-03-10 09:33:55 +0100

    Delete v0 quiz directory

--- v0 quiz/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Quiz } from "@/components/quiz"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-50 to-white p-4">
-      <Quiz />
-    </main>
-  )
-}
