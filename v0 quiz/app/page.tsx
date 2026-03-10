65c67498df3df8b3a06b57e890a12d5d64daa390
Stigamortis <clankacentral@gmail.com>
2026-03-10 09:31:01 +0100

    Add files via upload

---
+++ v0 quiz/app/page.tsx
@@ -0,0 +1,9 @@
+import { Quiz } from "@/components/quiz"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-50 to-white p-4">
+      <Quiz />
+    </main>
+  )
+}
